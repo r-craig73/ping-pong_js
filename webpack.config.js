d67b7422ded72721f63420163210c38befafd4b3
Ron Craig <ron.craig@comcast.net>
2018-03-12 00:01:24 -0700

    add ESLint, but CSS and errors are present

--- webpack.config.js
+++ webpack.config.js
@@ -25,11 +25,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
       }
     ]
   }
