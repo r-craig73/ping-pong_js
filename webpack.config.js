2c06d6f264a15ce918e30b9b04b47afd94a8a959
Ron Craig <ron.craig@comcast.net>
2018-10-09 19:14:14 -0700

    add CleanWebpack and Minfying-UglifyJS plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const CleanWebpackPlugin = require('clean-webpack-plugin');
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -11,6 +11,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
+    new UglifyJsPlugin(),
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Ping Pong',
       template: './src/index.html',
